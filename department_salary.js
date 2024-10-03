a0d0d06cc2cef22c672175e214e0727978b52994
manujunqueira <manuelajunqueira27@gmail.com>
2024-10-03 19:24:04 -0400

    "Create recursive function to calculate department salary"

--- department_salary.js
+++ department_salary.js
@@ -52,3 +52,30 @@ let company = {
         }
     ]
  }
+
+
+ // Task 2 - Create a Recursive Function to Calculate Total Salary for a Department
+
+ function calculateDepartmentSalary(departments){
+    let totalSalary = 0; //starting with total salary at 0 to sum the values
+
+    for (let employees of departments.employees){
+        totalSalary += employee.salary; // using for of to go through each employee again and sum their salaries 
+
+        if (employee.subordinates && employee.subordinates.lengh > 0){ // chacking if the imployee has subordinates; if so, it calculates their total salary 
+            totalSalary += calculateDepartmentSalary({employees: employee.subordinates});
+        }
+    }
+
+    return totalSalary; 
+
+ }
+
+ let businessTotalSalary = calculateDepartmentSalary(company.departments[0]); // calculate total salary for business department
+ console.log(`The total salary for the Business department is ${businessTotalSalary}.`); // console log the final value
+
+ let engineeringTotalSalary = calculateDepartmentSalary(company.departments[1]); // calculate total salary for engineering department
+ console.log(`The total salary for the Engineering department is ${engineeringTotalSalary}`); // console log the final value
+
+
+
